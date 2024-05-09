de6667e16cd584643fb0129bf3b9b5a3ea64bc59
KOSA <KOSA@host.docker.internal>
2024-05-09 09:19:54 +0900

    first commit

--- solve/src/num_14409/question_16719.java
+++ solve/src/num_14409/question_16719.java
@@ -7,5 +7,7 @@ public class question_16719 {
 
 	public static void main(String[] args) {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		
+		System.out.println("Hi");
 	}
 }
